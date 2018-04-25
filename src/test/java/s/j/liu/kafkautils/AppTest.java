3e9aed7a2d74a8c9a766252eb3e920a7a7870e5e
FSixteen <lshj01@yeah.net>
2018-04-25 15:06:21 +0800

    update

--- src/test/java/s/j/liu/kafkautils/AppTest.java
+++ src/test/java/s/j/liu/kafkautils/AppTest.java
@@ -13,6 +13,13 @@ import junit.framework.TestSuite;
 public class AppTest extends TestCase {
 
   /**
+   * kafka-topics.sh --create --zookeeper 172.16.42.5:2181 --replication-factor
+   * 2 --partitions 12 --topic car </br>
+   * kafka-topics.sh --delete --zookeeper 172.16.42.5:2181 --replication-factor
+   * 2 --partitions 12 --topic car </br>
+   */
+
+  /**
    * 测试.
    * 
    * @param args
@@ -21,8 +28,7 @@ public class AppTest extends TestCase {
   public static void main(String[] args) {
     boolean s = true;
     if (s) {
-      KafkaProducerUtils u = new KafkaProducerUtils("192.168.56.2:9092,192.168.56.3:9092,"
-          + "192.168.56.4:9092");
+      KafkaProducerUtils u = new KafkaProducerUtils("192.168.56.2:9092,192.168.56.3:9092,192.168.56.4:9092");
       List<String> ls = new ArrayList<String>();
       long l = System.currentTimeMillis();
       for (int i = 0; i < 1000; i++) {
@@ -32,8 +38,7 @@ public class AppTest extends TestCase {
       long m = System.currentTimeMillis();
       System.out.println(m - l); // 551
     } else {
-      KafkaProducerUtils u = new KafkaProducerUtils("192.168.56.2:9092,192.168.56.3:9092,"
-          + "192.168.56.4:9092");
+      KafkaProducerUtils u = new KafkaProducerUtils("192.168.56.2:9092,192.168.56.3:9092,192.168.56.4:9092");
       long l = System.currentTimeMillis();
       for (int i = 0; i < 10000; i++) {
         u.sendOneMessage("test", String.valueOf(i));
